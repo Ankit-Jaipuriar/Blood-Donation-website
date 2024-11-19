6b503036d86b053f2f7abff4b77ff833613098ab
Ankit Kumar Jaipuriar <ankitjaipuriar07@gmail.com>
2024-11-19 15:51:58 +0530

    Update footer-component.jsx

--- src/components/sections/footer/footer-component.jsx
+++ src/components/sections/footer/footer-component.jsx
@@ -103,13 +103,12 @@ const FooterComponent = () => {
 				</div>
 				<div className="footer-col fourth-col text-center mt-10 border-t-[1px] border-off_white/[.2] pt-6">
 					<h3 className="not-italic text-center font-regular text-[18px] leading-[34px] text-[#D9D9D9] ">
-						©️ 2023 HemoCell - Website design by{" "}
+						©️ 2024 HemoCell
 						<a
 							href="https://linkedin.com/in/moazamdev"
 							className="underline"
 							target="_blank"
 						>
-							AlphaDev
 						</a>
 					</h3>
 				</div>
